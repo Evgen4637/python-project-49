2d81a8593f316db79c85ac3d6817541a41b592d7
Evgen4637 <surmin.work@gmail.com>
2025-01-13 23:31:25 +0300

    modify def welcome_user()

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,6 +2,5 @@ import prompt
 
 
 def welcome_user():
-
-    name = prompt.string('May I have your name? ')
-    return print('Hello, ' + name)
+    user_name = prompt.string('May I have your name? ')
+    return user_name
